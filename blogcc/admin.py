577ff0a74d40b107a56ff9fa15adcd94c7d82b38
rockymiss <rachelmcdermottrock@gmail.com>
2022-09-09 17:41:33 +0000

    Create class to display Blog Comments in Admin Panel

--- blogcc/admin.py
+++ blogcc/admin.py
@@ -2,7 +2,7 @@
 
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import BlogPost
+from .models import BlogPost, BlogComment
 
 
 @admin.register(BlogPost)
@@ -25,3 +25,22 @@ class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('blog_content')
 
 
+@admin.register(BlogComment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Applies display, filter, search and action functionality
+    to the BlogPost Model on the admin panel
+    """
+    list_display = ('name',
+                    'comment_created',
+                    'comment_body',
+                    'approve')
+    list_filter = ('approve', 'comment_created')
+    search_fields = ('name', 'comment_body')
+    actions = ['approve_comments']
+
+    def comment_approve(self, request, queryset):
+        """
+        Creates an action which allows admin to approve a comment
+        """
+        queryset.update(approve=True)
