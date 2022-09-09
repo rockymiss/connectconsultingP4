56ce3d30ea381929e243ef3402a4c7e8da0f6dc7
rockymiss <rachelmcdermottrock@gmail.com>
2022-09-09 18:38:18 +0000

    Create url for home page and link to index.html, remove logo from base for now

--- blogcc/views.py
+++ blogcc/views.py
@@ -1,3 +1,13 @@
+"""Views"""
+
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+
+class HomePageView(TemplateView):
+    """
+    Creates view for Home Page
+    """
+    template_name = "index.html"
--- blogconnect/urls.py
+++ blogconnect/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('', include('blogcc.urls'), name='blogcc_urls'),
 ]
