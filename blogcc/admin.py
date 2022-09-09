70d7bad633fb9dcc14bc6f4805c283b91ae25562
rockymiss <rachelmcdermottrock@gmail.com>
2022-09-09 17:22:06 +0000

    Create class for Admin Panel to add blog post to inc summernote + url connect

--- blogcc/admin.py
+++ blogcc/admin.py
@@ -1,3 +1,21 @@
-from django.contrib import admin
+"""Admin Panel"""
 
-# Register your models here.
+from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import BlogPost
+
+
+@admin.register(BlogPost)
+class PostAdmin(SummernoteModelAdmin):
+    """
+    Applies display, filter, search, prepopulated fields
+    and summernote functionality to the BlogPost Model
+    on the admin panel
+
+    """
+
+    search_fields = ['blog_title', 'blog_content']
+    list_display = ('blog_title', 'status', 'blog_created_on', 'author')
+    list_filter = ('status', 'blog_created_on')
+    prepopulated_fields = {'slug': ('blog_title',)}
+    summernote_fields = ('blog_content')
