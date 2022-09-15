32ceaca1190852ccbd585c30df88dfc46bf2d4dc
rockymiss <rachelmcdermottrock@gmail.com>
2022-09-15 16:46:05 +0000

    Link conact app urls to project

--- blogconnect/urls.py
+++ blogconnect/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('accounts/', include('allauth.urls')),
+    path('', include('contact.urls'), name='contact_urls'),
     
 ]
