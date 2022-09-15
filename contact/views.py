2682bf4a2a16a4392f1b8306ff8f427a1f6b6e40
rockymiss <rachelmcdermottrock@gmail.com>
2022-09-15 21:22:24 +0000

    Add Listview and deleteview for messages

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,9 @@
 """
 Views for Contact App
 """
-from django.views.generic import CreateView, TemplateView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import CreateView, TemplateView, ListView, DeleteView
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.urls import reverse_lazy
 from .models import ContactUs
 from .forms import ContactForm
@@ -24,3 +26,60 @@ class ThankYouView(TemplateView):
     Creates view for Thank You Page
     """
     template_name = "contactus/thankyou.html"
+
+
+class MessageView(UserPassesTestMixin, ListView):
+    """
+    Checks to see if user is superuser, gets a list of
+    Messages made by user 
+    """
+
+    def test_func(self):
+        """
+        Checks if superuser
+        """
+        return self.request.user.is_superuser
+
+    template_name = 'contactus/getmessage.html'
+    model = ContactUs
+    queryset = ContactUs.objects.order_by('date_created')
+    context_object_name = 'getmessage'
+
+
+class DeleteMessage(UserPassesTestMixin, DeleteView):
+    """
+    Checks to see if user is admin and allows admin
+    to delete a message sent by the user 
+    """
+    def test_func(self):
+        """
+        Checks if superuser
+        """
+        return self.request.user.is_superuser
+
+    def get(self, request, pk, *args, **kwargs):
+        """
+        gets the object instance's id and assigns primary key
+        """
+        getmessage = get_object_or_404(ContactUs, pk=pk)
+        context = {
+            'getmessage': getmessage,
+        }
+
+        return render(
+            request,
+            'contactus/message_confirm_delete.html',
+            context
+        )
+
+    def post(self, request, pk, *args, **kwargs):
+        """
+        gets the message the user made Admin can
+        then delete the content
+        """
+
+        getmessage = get_object_or_404(ContactUs, pk=pk)
+        getmessage.delete()
+
+        return redirect('getmessage')
+
