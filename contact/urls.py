7059cca5e9b96699a64f290288a4529151946cfd
rockymiss <rachelmcdermottrock@gmail.com>
2022-09-15 21:21:55 +0000

    Add urls for getmessage and message delete

--- contact/urls.py
+++ contact/urls.py
@@ -2,10 +2,12 @@
 urls for blog app
 """
 from django.urls import path
-from .views import ContactCreate, ThankYouView
+from .views import ContactCreate, ThankYouView, MessageView, DeleteMessage
 
 
 urlpatterns = [
-    path('contactus/contact', ContactCreate.as_view(), name="contact"),
-    path('contactus/thankyou', ThankYouView.as_view(), name="thankyou")
+    path('contact/', ContactCreate.as_view(), name="contact"),
+    path('thankyou/', ThankYouView.as_view(), name="thankyou"),
+    path('getmessage/', MessageView.as_view(), name="getmessage"),
+    path('contactus/message_confirm_delete/<int:pk>/', DeleteMessage.as_view(), name="message_confirm_delete"),
 ]
