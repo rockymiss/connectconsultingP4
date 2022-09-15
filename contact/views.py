d8b5dc05e01ae5b41dee5be41112ba6ed51c06bf
rockymiss <rachelmcdermottrock@gmail.com>
2022-09-15 19:16:08 +0000

    Create views for Contact and Thank You pages

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+"""
+Views for Contact App
+"""
+from django.views.generic import CreateView, TemplateView
+from django.urls import reverse_lazy
+from .models import ContactUs
+from .forms import ContactForm
 
-# Create your views here.
+
+class ContactCreate(CreateView):
+    """
+    Creates a view so that users 
+    can fill out a contact form to 
+    contact the owners
+    """
+    model = ContactUs
+    template_name = 'contactus/contact.html'
+    form_class = ContactForm
+    success_url = reverse_lazy("thankyou")
+
+
+class ThankYouView(TemplateView):
+    """
+    Creates view for Thank You Page
+    """
+    template_name = "contactus/thankyou.html"
