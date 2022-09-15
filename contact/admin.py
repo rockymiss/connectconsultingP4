fd75c90a14dc1e9801cff251fd6b7319b601c998
rockymiss <rachelmcdermottrock@gmail.com>
2022-09-15 19:14:01 +0000

    Register admin model for Contact us

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from contact.models import ContactUs
 
 # Register your models here.
+
+
+@admin.register(ContactUs)
+class ContactAdmin(admin.ModelAdmin):
+    """
+    Applies display, filter, search and action functionality
+    to the BlogPost Model on the admin panel
+    """
+    list_display = ('first_name',
+                    'last_name',
+                    'email',
+                    'message',
+                    'date_created')
+    list_filter = ('email', 'date_created')
+    search_fields = ('email', 'message')
